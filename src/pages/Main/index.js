daec7a92c223466c9dfbfaec3ffea66b3c1bf92d
Cristiano Ferreira Mothe <cristianodevsystem@gmail.com>
2024-06-21 22:59:05 -0300

    feat: Pesquisando repositórios e usando o useCallback

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,25 +1,55 @@
-import React from "react";
-import { FaGithub, FaPlus } from "react-icons/fa"
+import React, { useState, useCallback } from "react";
+import { FaGithub, FaPlus } from "react-icons/fa";
 
-import { Container, Form, SubmitButton } from './styles'
+import { Container, Form, SubmitButton } from "./styles";
+
+import api from "../services/api";
 
 const Main = () => {
+  const [newRepo, setNewRepo] = useState("");
+  const [repositories, setRepositories] = useState([]);
+
+  const handleSubmit = useCallback((event) => {
+    event.preventDefault();
+
+    const submit = async () => {
+      const response = await api.get(`repos/${newRepo}`);
+
+      const data = {
+        name: response.data.full_name,
+      };
+
+      setRepositories([...repositories, data]);
+      setNewRepo("");
+    }
+
+    submit()
+
+  }, [newRepo, repositories]);
+
+  const handleInputChange = (event) => {
+    setNewRepo(event.target.value);
+  };
+
   return (
     <Container>
-      
       <h1>
         <FaGithub size={25} />
         Meus Repositórios
       </h1>
 
-      <Form>
-        <input type="text" placeholder="Adicionar Repositório" />
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Adicionar Repositório"
+          value={newRepo}
+          onChange={handleInputChange}
+        />
 
         <SubmitButton>
-          <FaPlus size={14} color="#FFF"/>
+          <FaPlus size={14} color="#FFF" />
         </SubmitButton>
       </Form>
-
     </Container>
   );
 };
